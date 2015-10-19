9a3874bb17f2b266ad29bbcc4739fb25a1b7f8ef
coldmooon <liy@bupt.edu.cn>
2015-10-19 12:32:26 +0800

    add new file: shell_order.sh

---
+++ shell_order.sh
@@ -0,0 +1,3 @@
+sudo echo "echo \"*** noe executing /etc/profile ***\"" >> /etc/profile
+echo "echo \"*** now executing ~/.bashrc ***\"" >> ~/.bashrc
+echo "echo \"*** now executing ~/.profile ***\"" >> ~/.profile
