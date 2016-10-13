14487a493791df217c9e72a15cac457707e2a8d4
coldmooon <liy@bupt.edu.cn>
2016-10-13 20:18:07 +0800

    Fixed a bug in opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,10 +1,10 @@
 # Compile opencv code in command line.
 # 
 echo "compiling $1"
-if [[ $1 == *.c ]]
+if [[ "$1 == *.c" ]]
 then
     gcc -g `pkg-config --cflags opencv`  -o `dirname $1`/`basename $1 .c` $1 `pkg-config --libs opencv`;
-elif [[ $1 == *.cpp ]]
+elif [[ "$1 == *.cpp" ]]
 then
     g++ -g `pkg-config --cflags opencv` -std=c++11 -std=gnu++11 -o `dirname $1`/`basename $1 .cpp` $1 `pkg-config --libs opencv`;
 else
