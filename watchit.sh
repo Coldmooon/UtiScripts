9b75b186e037c4b1cfe6578861742ff551acb209
Coldmooon <howtoliy@gmail.com>
2023-10-13 10:01:35 +0800

    Update watchit.sh

--- watchit.sh
+++ watchit.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Before run this script. Please check the following things:
+#   1. Please check your conda env. Make sure that the command you are going to run is under the desired conda env.
+#   2. Please check if the command you are going to run has the execute permission. If it doesn't have the execute permission, use `chmod +x` to grant it.
+
 PID=$1
 TODO=$2
 while [[ ( -d /proc/$PID ) && ( -z `grep zombie /proc/$PID/status` ) ]]; do
